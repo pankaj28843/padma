39a7083bdfc4e82aba0bd579f7fa1a52446608b1
vnagarjuna <vnagarjuna>
2005-11-11 14:00:04 +0000

    Add ability to change a script by rightclicing on it in the whitelist dialog

--- src/content/whitelist.js
+++ src/content/whitelist.js
@@ -1,4 +1,4 @@
-// $Id: whitelist.js,v 1.4 2005/11/05 16:26:06 vnagarjuna Exp $ -->
+// $Id: whitelist.js,v 1.5 2005/11/11 14:00:04 vnagarjuna Exp $ -->
 
 //Copyright 2005 Nagarjuna Venna <vnagarjuna@yahoo.com>
 
@@ -39,14 +39,18 @@ var PadmaWhitelist = {
         this.newSiteBox   = document.getElementById("autoSiteTextbox");
         this.whitelistBox = document.getElementById("autoSitelist");
         this.scriptList   = document.getElementById("autoScriptList");
+        this.scriptChangePopup = document.getElementById('changeScriptPopup');
         
         //Fill the script list with the menu items
         //Add Default always
         this.scriptList.appendItem(this.defaultLabel, this.defaultValue);
-        for(i = 0; i < Padma.script_MAXSCRIPTS; ++i)
+        this.scriptChangePopup.appendChild(this.getPopupMenuItem(this.defaultLabel, this.defaultValue));
+        for(var i = 0; i < Padma.script_MAXSCRIPTS; ++i) {
             this.scriptList.appendItem(Padma.scripts[i], i);
+            this.scriptChangePopup.appendChild(this.getPopupMenuItem(Padma.scripts[i], i));
+        }
         this.scriptList.selectedIndex = 0;
-        
+
         //Load site list
         if (this.params.input.length != 0) {
             var sites = this.params.input.split(",");
@@ -67,7 +71,7 @@ var PadmaWhitelist = {
             return false;
         }
         var rows = this.whitelistBox.getRowCount(), whitelist = "";
-        for(i = 0; i < rows; ++i) {
+        for(var i = 0; i < rows; ++i) {
             var item = this.whitelistBox.getItemAtIndex(i);
             var cell1 = item.firstChild;
             var index =  cell1.nextSibling.getAttribute(this.cellAttributeIndexName);
@@ -130,7 +134,7 @@ var PadmaWhitelist = {
             else this.removeButton.disabled = this.removeAllButton.disabled = true;
         }
         else this.whitelistBox.selectItem(this.whitelistBox.getItemAtIndex(index));
-    },
+   },
 
     onKeyPress: function (aEvent) {
         if (aEvent.keyCode == 46)
@@ -148,16 +152,35 @@ var PadmaWhitelist = {
         }
     },
 
-    appendItem: function(label1, label2) {
+    appendItem: function(label, value) {
         var item = document.createElement('listitem');
         var cell1 = document.createElement('listcell'), cell2 = document.createElement('listcell');
-        cell1.setAttribute('label', label1);
-        cell2.setAttribute('label', label2 == this.defaultValue ? this.defaultLabel : Padma.scripts[label2]);
-        cell2.setAttribute(this.cellAttributeIndexName, label2);
+        cell1.setAttribute('label', label);
+        cell2.setAttribute('label', value == this.defaultValue ? this.defaultLabel : Padma.scripts[value]);
+        cell2.setAttribute(this.cellAttributeIndexName, value);
         item.appendChild(cell1);
         item.appendChild(cell2);
+        item.setAttribute('context', 'changeScriptPopup');
         this.whitelistBox.appendChild(item);
         return item;
+    },
+
+    getPopupMenuItem: function(label, value) {
+        var menuItem = document.createElement('menuitem');
+        menuItem.setAttribute('label', label);
+        menuItem.setAttribute('oncommand', 'PadmaWhitelist.onChangeScript(' + value + ')');
+        return menuItem;
+    },
+
+    onChangeScript: function(value) {
+        var index = this.whitelistBox.selectedIndex;
+        if (index == -1)
+            return;
+
+        var selItem = this.whitelistBox.selectedItem;
+        var cell2   = selItem.firstChild.nextSibling;;
+        cell2.setAttribute('label', value == this.defaultValue ? this.defaultLabel : Padma.scripts[value]);
+        cell2.setAttribute(this.cellAttributeIndexName, value);
     },
 
     //TODO: sort
