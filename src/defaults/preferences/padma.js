e796c34d3dee6d789a78a45b1bc6f792dc0b335b
vnagarjuna <vnagarjuna>
2005-12-05 18:11:11 +0000

    More auto transform URL's

--- src/defaults/preferences/padma.js
+++ src/defaults/preferences/padma.js
@@ -1,6 +1,6 @@
 pref("Padma.RTSOutputFormat", 0);
 pref("Padma.RTSSunnaStyle", 0);
-pref("Padma.autoTransformWhiteList", "www.eenadu.net:-1,www.andhraprabha.com:-1,www.andhrajyothy.com:-1,www.webprapancham.com:-1,www.teluguone.com:-1,www.shirdisaiashirvadam.org:-1,www.apweekly.com:-1,www.keralahistory.ac.in:-1,www.deepika.com:-1,www.mangalam.com:-1,www.vaarttha.com:-1,thatstelugu.indiainfo.com:-1,www.kaburlu.com:-1,www.madhyamamonline.in:-1,www.deshabhimani.com:-1,www.manoramaonline.com:-1,www.esakal.com:-1,www.kumudam.com:-1,www.dinamalar.com:-1,www.dailythanthi.com:-1,www.vikatan.com:-1,www.dainikbhaskar.com:-1");
+pref("Padma.autoTransformWhiteList", "www.eenadu.net:-1,www.andhraprabha.com:-1,www.andhrajyothy.com:-1,www.webprapancham.com:-1,www.teluguone.com:-1,www.shirdisaiashirvadam.org:-1,www.apweekly.com:-1,www.keralahistory.ac.in:-1,www.deepika.com:-1,www.mangalam.com:-1,www.vaarttha.com:-1,thatstelugu.indiainfo.com:-1,www.kaburlu.com:-1,www.madhyamamonline.in:-1,www.deshabhimani.com:-1,www.manoramaonline.com:-1,www.esakal.com:-1,www.kumudam.com:-1,www.dinamalar.com:-1,www.dailythanthi.com:-1,www.vikatan.com:-1,www.bhaskar.com:-1,www.jagran.com:-1,www.gujaratsamachar.com:-1,www.rajasthanpatrika.com:-1,www.amarujala.com:-1,www.keralakaumudi.com:-1,www.aponline.gov.in:-1,www.punjabkesari.com:-1");
 pref("Padma.enableAutoTransform", true);
 pref("Padma.enableISCII", true);
 pref("Padma.enableITRANS", true);
