f05c53ce1db125a78edc26245c796aba98be065e
vnagarjuna <vnagarjuna>
2006-09-26 19:05:23 +0000

    Bump version numbers for 2.0 release

--- src/content/version.js
+++ src/content/version.js
@@ -1,4 +1,4 @@
-// $Id: version.js,v 1.7 2006/03/24 21:17:43 vnagarjuna Exp $ -->
+// $Id: version.js,v 1.8 2006/09/26 19:05:23 vnagarjuna Exp $ -->
 
 //Copyright 2005-2006 Nagarjuna Venna <vnagarjuna@yahoo.com>
 
@@ -24,7 +24,7 @@
 //Upgrade from version to version
 var Padma_Version = {
 
-    curVersion    : '0.4.7',
+    curVersion    : '0.4.8',
 
     getCurVersion : function() {
         return this.curVersion;
